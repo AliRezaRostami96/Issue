eefbe85a07abe02102c771b72a3f0200a86e591b
AliReza Rostami <aliirezaa96@gmail.com>
2021-10-30 12:59:15 +0330

    update side menu, check from localStrorage

--- public/js/js.js
+++ public/js/js.js
@@ -1,10 +1,25 @@
+$(document).ready(function() {
+    console.log("ready");
+    checkMenuStatus();
+})
+
+
+function checkMenuStatus() {
+    // check from localstorage
+    const menuStatus = localStorage.getItem("menuStatus");
+    if(menuStatus == "menu-is-open") {
+        menuToggle();
+    }
+}
 function menuToggle() {
     let menuStatus = $(".main-menu").hasClass("menu-is-open");
     if(menuStatus) {
         $(".main-menu").removeClass("menu-is-open");
         $(".main-content").removeClass("menu-is-open");
+        localStorage.setItem("menuStatus", null);
     } else {
         $(".main-menu").addClass("menu-is-open");
         $(".main-content").addClass("menu-is-open");
+        localStorage.setItem("menuStatus", "menu-is-open");
     }
 }
